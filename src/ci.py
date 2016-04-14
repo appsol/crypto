eb5b4b0c69668c7efb173b4fbd09011ade74967b
Stuart Laverick <appsol@gmail.com>
2016-04-14 23:12:12 +0100

    Completed calculation of CI

--- src/ci.py
+++ src/ci.py
@@ -10,8 +10,13 @@ def init():
     if cypherTxt is not None:
         cypherTxt = utils.stripWhiteSpace(cypherTxt)
         cypherTxtLength = len(cypherTxt)
+        print cypherTxtLength
         charFreq = characterCounts(cypherTxt)
         print charFreq
+        sumFreq = sumOfFrequencies(charFreq)
+        print sumFreq
+        ci = sumFreq / (cypherTxtLength * (cypherTxtLength - 1))
+        print ci
 
 
 def characterCounts(txt):
@@ -19,6 +24,12 @@ def characterCounts(txt):
     for x in txt.lower():
         freq[ord(x) - 97] += 1
     return freq
+
+def sumOfFrequencies(charFreq):
+	freqTotal = 0.0
+	for n in charFreq:
+		freqTotal+= n * (n - 1)
+	return freqTotal
 
 if __name__ == "__main__":
     init()
