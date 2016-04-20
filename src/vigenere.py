8cc293fcb7e5ae5911afa39d06f4ae36ba56fbd0
Stuart Laverick <appsol@gmail.com>
2016-04-20 10:21:23 +0100

    Added more comments to vigeres.py

--- src/vigenere.py
+++ src/vigenere.py
@@ -17,9 +17,12 @@ def init():
 
 
 def calculateIoCTable(text, maxPeriod=20):
+    '''Create a dictionary with the Index of Coincidence for
+    each key length up to max of the second parameter (default 20)'''
     iocTable = {}
     for n in xrange(2, maxPeriod):
         iocSum = 0
+        # Get mean ioc for all n period characters up to current key length (n)
         for offset in xrange(0, n-1):
             nChars = getNPeriodCharacters(n, offset, text)
             iocSum += indexOfCoincidence(''.join(nChars))
@@ -28,6 +31,7 @@ def calculateIoCTable(text, maxPeriod=20):
 
 
 def getNPeriodCharacters(n, offset, text):
+    '''Create list of nth characters (with a given offset) from the text'''
     textLength = len(text)
     index = offset
     nthCharacters = []
@@ -38,18 +42,27 @@ def getNPeriodCharacters(n, offset, text):
 
 
 def estimateKeyLength(iocTable):
+    '''Return the most likely key length from the Index of Coincidence dictionary.
+    Key lengths are scored on their Index of Coincidence, with additional points
+    for being a factor of one of the top 3 key length candidates'''
+    # Sort the key length IOC dictionary descending
     sortedIoC = sorted(iocTable.iteritems(), key=operator.itemgetter(1), reverse=True)
+    # Just need the sorted key lengths
     keys = [x for x, y in sortedIoC]
     candidates = {}
     score = 3
+    # Iterate over the top 3
     for x in keys[:3]:
+        # Apply reducing score
         candidates[x] = score
         score -= 1
+        # If they are a factor of another candidate add to score
         for y in keys[:3]:
             if x == y:
                 continue
             if y % x == 0:
                 candidates[x] += 1
+    # Return the highest scoring
     return max(candidates.iteritems(), key=operator.itemgetter(1))[0]
 
 
